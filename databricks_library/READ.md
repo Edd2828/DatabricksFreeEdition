02f4639c908c28b839d75cd19127ddaaaae786bb
Edward Colmer <Edward.Colmer@innovation.group>
2026-06-11 13:23:03 +0100

    custom library for databricks free edition

---
+++ databricks_library/READ.md
@@ -0,0 +1,10 @@
+# Library instructions usage
+
+### Build Library whl file
+```cmd
+python setup.py sdist bdist_wheel 
+```
+### Install library
+```cmd
+pip install dist/databricks_library-0.0.1-py3-none-any.whl
+```
---
+++ databricks_library/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='databricks_library',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Edd2828',
+    description='A simple utility for databricks free edition',
+    # long_description=open('READ.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Edd2828/DatabricksFreeEdition/databricks_library'
+)
